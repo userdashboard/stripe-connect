4c3ace91368c4f5d84ebee26df8b874873f5b280
Ben Lowry <benlowry@outlook.com>
2019-08-20 08:37:37 -0600

    Replaced legacy invalid-documentid with front/back errors

--- src/www/api/user/connect/set-individual-registration-submitted.js
+++ src/www/api/user/connect/set-individual-registration-submitted.js
@@ -19,8 +19,10 @@ module.exports = {
       throw new Error('invalid-payment-details')
     }
     const registration = connect.MetaData.parse(stripeAccount.metadata, 'registration')
-    if (!registration || !registration['individual_verification_document_front'] || !registration['individual_verification_document_back']) {
-      throw new Error('invalid-documentid')
+    if (!registration || !registration['individual_verification_document_front']) {
+      throw new Error('invalid-individual_verification_document_front')
+    } if (!registration['individual_verification_document_back']) {
+      throw new Error('invalid-individual_verification_document_back')
     }
     req.query.country = stripeAccount.country
     const countrySpec = await global.api.user.connect.CountrySpec.get(req)
