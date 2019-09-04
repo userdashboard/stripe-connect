25096db5fd9f3b628bac6a82a9452f7703af2a6b
Ben Lowry <benlowry@outlook.com>
2019-09-04 11:19:53 -0600

    Updated dependencies

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 global.stripeAPIVersion = '2019-08-14'
 
+const packageJSON = require('./package.json')
+const stripe = require('stripe')()
+stripe.setApiVersion(global.stripeAPIVersion)
+stripe.setAppInfo({
+  version: packageJSON.version,
+  name: '@userdashboard/stripe-connect',
+  url: 'https://github.com/userdashboard/stripe-connect'
+})
+
 module.exports = {
   MetaData: require('./src/meta-data.js')
 }
