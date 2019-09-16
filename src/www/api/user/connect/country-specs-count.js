2b52c631ecafddd9da0db132fb9f9823bbc4f7a5
benlowry <benlowry@outlook.com>
2019-09-16 17:24:09 -0600

    Added count route for consistency

---
+++ src/www/api/user/connect/country-specs-count.js
@@ -0,0 +1,16 @@
+const stripe = require('stripe')()
+stripe.setApiVersion(global.stripeAPIVersion)
+stripe.setMaxNetworkRetries(global.maximumStripeRetries)
+let cache
+
+module.exports = {
+  get: async (req) => {
+    if (!cache) {
+      const items = await stripe.countrySpecs.list({ limit: 100 }, req.stripeKey)
+      if (items && items.length) {
+        cache = items.length
+      }
+    }
+    return cache
+  }
+}
