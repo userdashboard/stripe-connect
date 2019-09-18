da10707cf2b4eff2cc080c4b2285d2997737ebc7
benlowry <benlowry@outlook.com>
2019-09-18 10:14:28 -0600

    Removed dev dependencies

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,7 +1,5 @@
-npm update @userdashboard/dashboard || exit 1
 npm update @userdashboard/maxmind-geoip || exit 1
 npm update stripe || exit 1
-npm update puppeteer || exit 1
 npm shrinkwrap --dev || exit 1
 git add npm-shrinkwrap.json || exit 1
 git add package.json || exit 1
