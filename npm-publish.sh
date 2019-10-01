b7eea8ab471dfb856f2f0b5222602d4afa9047fa
benlowry <benlowry@outlook.com>
2019-10-01 08:16:21 -0600

    Increment version only if update not in progress

--- npm-publish.sh
+++ npm-publish.sh
@@ -1,4 +1,14 @@
-VERSION=`npm version patch` || exit 1
+LATEST=`npm info | grep latest`
+LATEST="${LATEST/latest/}"
+LATEST="${LATEST/: /}"
+PACKAGE=`cat package.json | grep version`
+PACKAGE="${PACKAGE/\"version\": \"/}"
+PACKAGE="${PACKAGE/\",/}"
+if [ "$LATEST" = "$PACKAGE" ]; then
+  VERSION=`npm version patch` || exit 1
+else
+  VERSION=$PACKAGE
+fi
 bash test.sh > tests.txt || exit 1
 FAST_START=true NODE_ENV=sitemap node main.js || exit 1
 git add . || exit 1
