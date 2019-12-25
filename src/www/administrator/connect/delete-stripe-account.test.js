b3edfa6b3b0fd9bd5e741ef445084ce68824eaba
benlowry <benlowry@outlook.com>
2019-12-25 22:16:51 +0000

    Debugging github actions with puppeteer

--- src/www/administrator/connect/delete-stripe-account.test.js
+++ src/www/administrator/connect/delete-stripe-account.test.js
@@ -112,23 +112,6 @@ describe('/administrator/connect/delete-stripe-account', () => {
         country: 'US',
         type: 'individual'
       })
-      await TestHelper.createStripeRegistration(user, {
-        business_profile_mcc: '7997',
-        business_profile_url: 'https://www.' + user.profile.contactEmail.split('@')[1],
-        individual_address_city: 'New York',
-        individual_address_line1: '285 Fulton St',
-        individual_address_postal_code: '10007',
-        individual_address_state: 'NY',
-        individual_dob_day: '1',
-        individual_dob_month: '1',
-        individual_dob_year: '1950',
-        individual_email: user.profile.contactEmail,
-        individual_first_name: user.profile.firstName,
-        individual_id_number: '000000000',
-        individual_last_name: user.profile.lastName,
-        individual_phone: '456-789-0123',
-        individual_ssn_last_4: '0000'
-      })
       const req = TestHelper.createRequest(`/administrator/connect/delete-stripe-account?stripeid=${user.stripeAccount.id}`)
       req.account = administrator.account
       req.session = administrator.session
@@ -141,6 +124,10 @@ describe('/administrator/connect/delete-stripe-account', () => {
         { click: `/administrator/connect/delete-stripe-account?stripeid=${user.stripeAccount.id}` },
         { fill: '#submit-form' }
       ]
+      console.log('administrator deleting user')
+      console.log(administrator.toString())
+      console.log('-----------')
+      console.log(user.toString())
       await req.post()
       const req2 = TestHelper.createRequest(`/api/administrator/connect/stripe-account?stripeid=${user.stripeAccount.id}`)
       req2.account = administrator.account
