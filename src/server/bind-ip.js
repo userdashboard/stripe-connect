b2c6629a746ee4c3f425a7067665a63cf3044d7c
benlowry <benlowry@outlook.com>
2019-12-25 21:44:00 +0000

    Bind IP to requests

---
+++ src/server/bind-ip.js
@@ -0,0 +1,26 @@
+module.exports = {
+  before: async (req) => {
+    req.ip = requestIPAddress(req)
+  }
+}
+
+function requestIPAddress (req) {
+  const xForwardFor = req.headers['x-forwarded-for']
+  if (xForwardFor) {
+    const comma = xForwardFor.indexOf(',')
+    if (comma > -1) {
+      return xForwardFor.substring(0, comma)
+    }
+    return xForwardFor
+  }
+  if (req.connection) {
+    if (req.connection.remoteAddress) {
+      return req.connection.remoteAddress
+    } else if (req.connection.socket) {
+      return req.connection.socket
+    }
+  }
+  if (req.socket && req.socket.remoteAddress) {
+    return req.socket.remoteAddress
+  }
+}
