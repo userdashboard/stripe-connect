97db8e936f4373424ce2d964db4201a134f90dd0
benlowry <benlowry@outlook.com>
2019-12-25 20:49:40 +0000

    Switched to synchronous initialization

--- index.js
+++ index.js
@@ -10,82 +10,64 @@ if (global.stripeJS > 0 && !global.stripePublishableKey) {
   throw new Error('invalid-stripe-publishable-key')
 }
 
-(async () => {
-  const packageJSON = require('./package.json')
-  const stripe = require('stripe')()
-  stripe.setApiVersion(global.stripeAPIVersion)
-  stripe.setMaxNetworkRetries(global.maximumStripeRetries)
-  stripe.setAppInfo({
-    version: packageJSON.version,
-    name: '@userdashboard/stripe-connect',
-    url: 'https://github.com/userdashboard/stripe-connect'
-  })
-  const countryList = require('./countries.json')
-  const countryNameIndex = {}
-  for (const country of countryList) {
-    countryNameIndex[country.code] = country.name
+const packageJSON = require('./package.json')
+const stripe = require('stripe')()
+stripe.setApiVersion(global.stripeAPIVersion)
+stripe.setMaxNetworkRetries(global.maximumStripeRetries)
+stripe.setAppInfo({
+  version: packageJSON.version,
+  name: '@userdashboard/stripe-connect',
+  url: 'https://github.com/userdashboard/stripe-connect'
+})
+const countryList = require('./countries.json')
+const countryNameIndex = {}
+for (const country of countryList) {
+  countryNameIndex[country.code] = country.name
+}
+const fs = require('fs')
+const path = require('path')
+const countrySpecIndex = {}
+const countryCurrencyIndex = {}
+const kycRequirements = {}
+const countrySpecs = require('./stripe-country-specs.json')
+for (const countrySpec of countrySpecs) {
+  countrySpecIndex[countrySpec.id] = countrySpec
+  countryCurrencyIndex[countrySpec.id] = []
+  for (const currency in countrySpec.supported_bank_account_currencies) {
+    countryCurrencyIndex[countrySpec.id].push({ name: currency.toUpperCase(), currency, object: 'currency' })
   }
-  const fs = require('fs')
-  const os = require('os')
-  const path = require('path')
-  const cachedCountrySpecs = os.tmpdir() + '/stripe_country_specs.json'
-  const countrySpecIndex = {}
-  let countrySpecs
-  if (fs.existsSync(cachedCountrySpecs)) {
-    countrySpecs = require(cachedCountrySpecs)
-  } else {
-    countrySpecs = await stripe.countrySpecs.list({ limit: 100 }, { api_key: process.env.STRIPE_KEY })
-    countrySpecs = countrySpecs.data
-    fs.writeFileSync(cachedCountrySpecs, JSON.stringify(countrySpecs))
-  }
-  countrySpecs.sort((a, b) => {
-    a.name = countryNameIndex[a.id]
-    b.name = countryNameIndex[b.id]
-    return a.id.toLowerCase() > b.id.toLowerCase() ? 1 : -1
-  })
-  const countryCurrencyIndex = {}
-  const kycRequirements = {}
-  for (const countrySpec of countrySpecs) {
-    countrySpecIndex[countrySpec.id] = countrySpec
-    countryCurrencyIndex[countrySpec.id] = []
-    for (const currency in countrySpec.supported_bank_account_currencies) {
-      countryCurrencyIndex[countrySpec.id].push({ name: currency.toUpperCase(), currency, object: 'currency' })
-    }
-    const filePath = path.join(__dirname, `/kyc-requirements/${countrySpec.id}.json`)
-    if (!fs.existsSync(filePath)) {
-      continue
-    }
-    kycRequirements[countrySpec.id] = JSON.parse(fs.readFileSync(filePath).toString())
-  }
-  const countryDivisions = {}
-  const raw = require('./country-divisions.json')
-  for (const object in raw) {
-    countryDivisions[raw[object].code] = []
-    for (const item in raw[object].divisions) {
-      countryDivisions[raw[object].code].push({
-        object: 'option',
-        text: raw[object].divisions[item],
-        value: item
-      })
-    }
-    countryDivisions[raw[object].code].sort((a, b) => {
-      return a.text.toLowerCase() < b.text.toLowerCase() ? -1 : 1
+  kycRequirements[countrySpec.id] = require(`./kyc-requirements/${countrySpec.id}.json`)
+}
+
+const countryDivisions = {}
+const raw = require('./country-divisions.json')
+for (const object in raw) {
+  countryDivisions[raw[object].code] = []
+  for (const item in raw[object].divisions) {
+    countryDivisions[raw[object].code].push({
+      object: 'option',
+      text: raw[object].divisions[item],
+      value: item
     })
   }
-  
-  const merchantCategoryCodes = require('./merchant-category-codes.json')
-  module.exports = {
-    countryList,
-    countryDivisions,
-    countryNameIndex,
-    countrySpecs,
-    countrySpecIndex,
-    countryCurrencyIndex,
-    kycRequirements,
-    euCountries: ['AT', 'BE', 'DE', 'DK', 'EE', 'ES', 'FI', 'FR', 'GB', 'IE', 'IT', 'LU', 'LT', 'LV', 'NL', 'NO', 'PT', 'SE', 'SI', 'SK'],
-    getMerchantCategoryCodes: (language) => {
-      return merchantCategoryCodes[language || global.language] || merchantCategoryCodes.en
-    },
-    MetaData: require('./src/meta-data.js')
-  }
-})()
+  countryDivisions[raw[object].code].sort((a, b) => {
+    return a.text.toLowerCase() < b.text.toLowerCase() ? -1 : 1
+  })
+}
+
+const merchantCategoryCodes = require('./merchant-category-codes.json')
+
+module.exports = {
+  countryList: countryList,
+  countryDivisions: countryDivisions,
+  countryNameIndex: countryNameIndex,
+  countrySpecs: countrySpecs,
+  countrySpecIndex: countrySpecIndex,
+  countryCurrencyIndex: countryCurrencyIndex,
+  kycRequirements: kycRequirements,
+  euCountries: ['AT', 'BE', 'DE', 'DK', 'EE', 'ES', 'FI', 'FR', 'GB', 'IE', 'IT', 'LU', 'LT', 'LV', 'NL', 'NO', 'PT', 'SE', 'SI', 'SK'],
+  getMerchantCategoryCodes: (language) => {
+    return merchantCategoryCodes[language || global.language] || merchantCategoryCodes.en
+  },
+  MetaData: require('./src/meta-data.js')
+}
