503c82c07bab0947138e19903b4be0115e6fd46d
benlowry <benlowry@outlook.com>
2020-01-04 16:58:01 +0000

    Removed only

--- src/www/api/administrator/connect/account-stripe-accounts-count.test.js
+++ src/www/api/administrator/connect/account-stripe-accounts-count.test.js
@@ -2,7 +2,7 @@
 const assert = require('assert')
 const TestHelper = require('../../../../../test-helper.js')
 
-describe.only('/api/administrator/connect/account-stripe-accounts-count', () => {
+describe('/api/administrator/connect/account-stripe-accounts-count', () => {
   describe('exceptions', () => {
     describe('invalid-accountid', () => {
       it('missing querystring accountid', async () => {
