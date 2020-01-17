190dfe593d656c7ca2f80b27b44bce1ac7157b5c
benlowry <benlowry@outlook.com>
2020-01-17 17:28:14 +0000

    WIP fixing payout-enabled status on test accounts

--- src/www/api/administrator/connect/payouts-count.test.js
+++ src/www/api/administrator/connect/payouts-count.test.js
@@ -1,48 +1,14 @@
 /* eslint-env mocha */
 const assert = require('assert')
 const TestHelper = require('../../../../../test-helper.js')
+const TestStripeAccounts = require('../../../../../test-stripe-accounts.js')
 
 describe('/api/administrator/connect/payouts-count', () => {
   describe('returns', () => {
     it('integer', async () => {
       const administrator = await TestHelper.createOwner()
       for (let i = 0, len = global.pageSize + 1; i < len; i++) {
-        const user = await TestHelper.createUser()
-        await TestHelper.createStripeAccount(user, {
-          country: 'NZ',
-          type: 'individual'
-        })
-        await TestHelper.createStripeRegistration(user, {
-          business_profile_mcc: '7531',
-          business_profile_url: 'https://www.abcde.com',
-          address_city: 'Auckland',
-          address_line1: '123 Sesame St',
-          address_postal_code: '6011',
-          address_state: 'AUK',
-          dob_day: '1',
-          dob_month: '1',
-          dob_year: '1950',
-          email: user.profile.contactEmail,
-          first_name: user.profile.firstName,
-          last_name: user.profile.lastName,
-          phone: '456 789 0123'
-        })
-        await TestHelper.createExternalAccount(user, {
-          account_holder_name: `${user.profile.firstName} ${user.profile.lastName}`,
-          account_holder_type: 'individual',
-          account_number: '0000000010',
-          country: 'NZ',
-          currency: 'nzd',
-          routing_number: '110000'
-        })
-        await TestHelper.submitStripeAccount(user)
-        await TestHelper.waitForVerificationFailure(user)
-        await TestHelper.createStripeRegistration(user, null, {
-          verification_document_back: TestHelper['success_id_scan_back.png'],
-          verification_document_front: TestHelper['success_id_scan_front.png'],
-          verification_additional_document_back: TestHelper['success_id_scan_back.png'],
-          verification_additional_document_front: TestHelper['success_id_scan_front.png']
-        })
+        const user = await TestStripeAccounts.createSubmittedIndividual('NZ')
         await TestHelper.waitForPayoutsEnabled(user)
         await TestHelper.createPayout(user)
         await TestHelper.waitForPayout(administrator, user.stripeAccount.id, null)
--- src/www/api/administrator/connect/stripe-account-payouts-count.test.js
+++ src/www/api/administrator/connect/stripe-account-payouts-count.test.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 const assert = require('assert')
 const TestHelper = require('../../../../../test-helper.js')
+const TestStripeAccounts = require('../../../../../test-stripe-accounts.js')
 
 describe('/api/administrator/connect/stripe-account-payouts-count', () => {
   describe('exceptions', () => {
@@ -38,81 +39,11 @@ describe('/api/administrator/connect/stripe-account-payouts-count', () => {
   describe('returns', () => {
     it('integer', async () => {
       const administrator = await TestHelper.createOwner()
-      const user = await TestHelper.createUser()
-      await TestHelper.createStripeAccount(user, {
-        country: 'NZ',
-        type: 'individual'
-      })
-      await TestHelper.createStripeRegistration(user, {
-        business_profile_mcc: '7531',
-        business_profile_url: 'https://www.abcde.com',
-        address_city: 'Auckland',
-        address_line1: '123 Sesame St',
-        address_postal_code: '6011',
-        address_state: 'AUK',
-        dob_day: '1',
-        dob_month: '1',
-        dob_year: '1950',
-        email: user.profile.contactEmail,
-        first_name: user.profile.firstName,
-        last_name: user.profile.lastName,
-        phone: '456 789 0123'
-      })
-      await TestHelper.createExternalAccount(user, {
-        account_holder_name: `${user.profile.firstName} ${user.profile.lastName}`,
-        account_holder_type: 'individual',
-        account_number: '0000000010',
-        country: 'NZ',
-        currency: 'nzd',
-        routing_number: '110000'
-      })
-      await TestHelper.submitStripeAccount(user)
-      await TestHelper.waitForVerificationFailure(user)
-      await TestHelper.createStripeRegistration(user, null, {
-        verification_document_back: TestHelper['success_id_scan_back.png'],
-        verification_document_front: TestHelper['success_id_scan_front.png'],
-        verification_additional_document_back: TestHelper['success_id_scan_back.png'],
-        verification_additional_document_front: TestHelper['success_id_scan_front.png']
-      })
+      const user = await TestStripeAccounts.createSubmittedIndividual('NZ')
       await TestHelper.waitForPayoutsEnabled(user)
       await TestHelper.createPayout(user)
       await TestHelper.waitForPayout(administrator, user.stripeAccount.id, null)
-      const user2 = await TestHelper.createUser()
-      await TestHelper.createStripeAccount(user2, {
-        country: 'NZ',
-        type: 'individual'
-      })
-      await TestHelper.createStripeRegistration(user2, {
-        business_profile_mcc: '7531',
-        business_profile_url: 'https://www.abcde.com',
-        address_city: 'Auckland',
-        address_line1: '123 Sesame St',
-        address_postal_code: '6011',
-        address_state: 'AUK',
-        dob_day: '1',
-        dob_month: '1',
-        dob_year: '1950',
-        email: user2.profile.contactEmail,
-        first_name: user2.profile.firstName,
-        last_name: user2.profile.lastName,
-        phone: '456 789 0123'
-      })
-      await TestHelper.createExternalAccount(user2, {
-        account_holder_name: `${user2.profile.firstName} ${user2.profile.lastName}`,
-        account_holder_type: 'individual',
-        account_number: '0000000010',
-        country: 'NZ',
-        currency: 'nzd',
-        routing_number: '110000'
-      })
-      await TestHelper.submitStripeAccount(user2)
-      await TestHelper.waitForVerificationFailure(user2)
-      await TestHelper.createStripeRegistration(user2, null, {
-        verification_document_back: TestHelper['success_id_scan_back.png'],
-        verification_document_front: TestHelper['success_id_scan_front.png'],
-        verification_additional_document_back: TestHelper['success_id_scan_back.png'],
-        verification_additional_document_front: TestHelper['success_id_scan_front.png']
-      })
+      const user2 = await TestStripeAccounts.createSubmittedIndividual('NZ')
       await TestHelper.waitForPayoutsEnabled(user2)
       await TestHelper.createPayout(user2)
       await TestHelper.waitForPayout(administrator, user2.stripeAccount.id, null)
