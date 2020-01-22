c5ae1100c1c23b58a765b405d84bb737daa3c9a3
benlowry <benlowry@outlook.com>
2020-01-22 17:13:49 +0000

    Catch 'resource missing' error

--- src/stripe-cache.js
+++ src/stripe-cache.js
@@ -29,6 +29,9 @@ module.exports = {
         if (error.raw && error.raw.code === 'idempotency_key_in_use') {
           continue
         }
+        if (error.raw && error.raw.code === 'resource_missing') {
+          continue
+        }
         if (error.type === 'StripeConnectionError') {
           continue
         }
@@ -61,6 +64,9 @@ module.exports = {
           continue
         }
         if (error.raw && error.raw.code === 'idempotency_key_in_use') {
+          continue
+        }
+        if (error.raw && error.raw.code === 'resource_missing') {
           continue
         }
         if (error.type === 'StripeConnectionError') {
