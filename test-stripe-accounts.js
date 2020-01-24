53eea99b841fc5d7cdb036cde6b25c1a149c370c
benlowry <benlowry@outlook.com>
2020-01-24 14:17:26 +0000

    Debugging

--- test-stripe-accounts.js
+++ test-stripe-accounts.js
@@ -36,9 +36,10 @@ module.exports = {
     for (const field in individualData[country]) {
       individual[field] = individualData[country][field]
     }
+    await TestHelper.createStripeRegistration(user, individual)
     console.log('waiting for document account requirement', JSON.stringify(user.stripeAccount, null, '  '))
     await TestHelper.waitForAccountRequirement(user, 'individual.verification.document')
-    await TestHelper.createStripeRegistration(user, individual, {
+    await TestHelper.createStripeRegistration(user, {}, {
       verification_document_back: TestHelper['success_id_scan_back.png'],
       verification_document_front: TestHelper['success_id_scan_front.png']
     })
