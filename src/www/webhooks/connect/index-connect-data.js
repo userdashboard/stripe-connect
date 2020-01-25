50990c1c334eff972d3b20c32558146edc1c1282
benlowry <benlowry@outlook.com>
2020-01-25 20:37:46 +0000

    Removed duplicate assignment

--- src/www/webhooks/connect/index-connect-data.js
+++ src/www/webhooks/connect/index-connect-data.js
@@ -35,7 +35,6 @@ module.exports = {
     if (!stripeEvent) {
       return res.end()
     }
-    res.statusCode = 200
     if (global.testNumber) {
       global.webhooks.unshift(stripeEvent)
     }
