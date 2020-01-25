8f2f106dd57fd4d3add0507e65e1e225a2f29c26
benlowry <benlowry@outlook.com>
2020-01-25 20:12:32 +0000

    Check account exists

--- src/www/webhooks/connect/stripe-webhooks/payout.created.js
+++ src/www/webhooks/connect/stripe-webhooks/payout.created.js
@@ -4,16 +4,48 @@ const stripeCache = require('../../../../stripe-cache.js')
 module.exports = async (stripeEvent, req) => {
   const add = []
   const payout = stripeEvent.data.object
+  let exists
+  while (true) {
+    if (global.testEnded) {
+      return
+    }
+    try {
+      exists = await stripe.accounts.retrieve(payout.account, req.stripeKey)
+      if (exists) {
+        break
+      }
+      return
+    } catch (error) {
+      if (error.raw && error.raw.code === 'lock_timeout') {
+        continue
+      }
+      if (error.raw && error.raw.code === 'rate_limit') {
+        continue
+      }
+      if (error.raw && error.raw.code === 'idempotency_key_in_use') {
+        continue
+      }
+      if (error.type === 'StripeConnectionError') {
+        continue
+      }
+      return
+    }
+  }
+  let accountid
+  try {
+    accountid = await dashboard.Storage.read(`${req.appid}/map/stripeid/accountid/${stripeEvent.account}`)
+  } catch (error) {
+  }
+  if (!accountid) {
+    return
+  }
   await stripeCache.update(payout)
-  const accountid = await dashboard.Storage.read(`${req.appid}/map/stripeid/accountid/${stripeEvent.account}`)
   await dashboard.Storage.write(`${req.appid}/map/payoutid/stripeid/${payout.id}`, stripeEvent.account)
   add.push({ index: `${req.appid}/payouts`, value: payout.id })
   add.push({ index: `${req.appid}/bankAccount/payouts/${payout.destination}`, value: payout.id })
   add.push({ index: `${req.appid}/account/payouts/${accountid}`, value: payout.id })
   add.push({ index: `${req.appid}/stripeAccount/payouts/${stripeEvent.account}`, value: payout.id })
-  if (add.length) {
-    for (const item of add) {
-      await dashboard.StorageList.add(item.index, item.value)
-    }
+  for (const item of add) {
+    await dashboard.StorageList.add(item.index, item.value)
   }
 }
