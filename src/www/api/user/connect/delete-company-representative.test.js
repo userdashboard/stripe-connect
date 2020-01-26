c24273a909f15bfb0265726ac1c6b50b13df3bdc
benlowry <benlowry@outlook.com>
2020-01-26 00:03:50 +0000

    Removed unnecessary test

--- src/www/api/user/connect/delete-company-representative.test.js
+++ src/www/api/user/connect/delete-company-representative.test.js
@@ -107,48 +107,4 @@ describe('/api/user/connect/delete-company-representative', () => {
       assert.strictEqual(deleted, true)
     })
   })
-
-  describe('configuration', () => {
-    it('environment STRIPE_JS', async () => {
-      global.stripeJS = 3
-      const user = await TestHelper.createUser()
-      await TestHelper.createStripeAccount(user, {
-        country: 'US',
-        type: 'company'
-      })
-      const req2 = TestHelper.createRequest(`/account/connect/edit-company-representative?stripeid=${user.stripeAccount.id}`)
-      req2.waitOnSubmit = true
-      req2.account = user.account
-      req2.session = user.session
-      req2.uploads = {
-        verification_document_back: TestHelper['success_id_scan_back.png'],
-        verification_document_front: TestHelper['success_id_scan_front.png']
-      }
-      req2.body = {
-        address_city: 'New York',
-        address_country: 'US',
-        address_line1: '285 Fulton St',
-        address_postal_code: '10007',
-        address_state: 'NY',
-        dob_day: '1',
-        dob_month: '1',
-        dob_year: '1950',
-        email: user.profile.contactEmail,
-        first_name: user.profile.firstName,
-        last_name: user.profile.lastName,
-        phone: '456-789-0123',
-        relationship_executive: 'true',
-        relationship_title: 'Owner',
-        ssn_last_4: '0000'
-      }
-      await req2.post()
-      const req3 = TestHelper.createRequest(`/api/user/connect/delete-company-representative?stripeid=${user.stripeAccount.id}`)
-      req3.account = user.account
-      req3.session = user.session
-      req3.filename = __filename
-      req3.saveResponse = true
-      const deleted = await req3.delete()
-      assert.strictEqual(deleted, true)
-    })
-  })
 })
