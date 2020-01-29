aa4ac78bc5780200d7b151b6c3b4d51ba6fcef2d
benlowry <benlowry@outlook.com>
2020-01-29 18:45:58 +0000

    Debugging

--- src/www/webhooks/connect/stripe-webhooks/account.updated.js
+++ src/www/webhooks/connect/stripe-webhooks/account.updated.js
@@ -2,5 +2,9 @@ const stripeCache = require('../../../../stripe-cache.js')
 
 module.exports = async (stripeEvent) => {
   const account = stripeEvent.data.object
+  if (global.monitorStripeAccount === account.id) {
+    console.log('monitored account activity')
+    console.log(JSON.stringify(stripeEvent.data.previous_attributes, null, '  '))
+  }
   await stripeCache.delete(account.id)
 }
