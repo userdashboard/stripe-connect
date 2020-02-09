2132d61214e189938e4c158cef00d71c58ed151a
benlowry <benlowry@outlook.com>
2020-02-09 18:19:58 +0000

    Updated link reference

--- src/www/account/connect/navbar-stripe-account.js
+++ src/www/account/connect/navbar-stripe-account.js
@@ -19,10 +19,8 @@ module.exports = {
       editCompany.parentNode.removeChild(editCompany)
       const submitCompany = template.getElementById('navbar-submit-company')
       submitCompany.parentNode.removeChild(submitCompany)
-      const companyOwners = template.getElementById('navbar-beneficial-owners')
-      companyOwners.parentNode.removeChild(companyOwners)
-      const companyDirectors = template.getElementById('navbar-company-directors')
-      companyDirectors.parentNode.removeChild(companyDirectors)
+      const persons = template.getElementById('navbar-persons')
+      persons.parentNode.removeChild(persons)
     } else {
       const editIndividual = template.getElementById('navbar-edit-individual')
       editIndividual.parentNode.removeChild(editIndividual)
