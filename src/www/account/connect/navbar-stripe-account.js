e2ab819f071ad3e67a4bf3a807e08e165ac5c0ea
benlowry <benlowry@outlook.com>
2020-02-10 13:29:06 +0000

    Fix null exception

--- src/www/account/connect/navbar-stripe-account.js
+++ src/www/account/connect/navbar-stripe-account.js
@@ -2,16 +2,10 @@ module.exports = {
   setup: (doc, stripeAccount) => {
     const template = doc.getElementById('navbar')
     if (stripeAccount.metadata.submitted) {
-      const editCompany = template.getElementById('navbar-edit-company')
-      editCompany.parentNode.removeChild(editCompany)
       const submitCompany = template.getElementById('navbar-submit-company')
       submitCompany.parentNode.removeChild(submitCompany)
-      const editIndividual = template.getElementById('navbar-edit-individual')
-      editIndividual.parentNode.removeChild(editIndividual)
       const submitIndividual = template.getElementById('navbar-submit-individual')
       submitIndividual.parentNode.removeChild(submitIndividual)
-      const companyOwners = template.getElementById('navbar-beneficial-owners')
-      companyOwners.parentNode.removeChild(companyOwners)
       return
     }
     if (stripeAccount.business_type === 'individual') {
